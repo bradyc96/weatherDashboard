e3b463603e4e7c37b62512e295c32b9872b1fb64
Brady Collier <bradycollier.123@gmail.com>
2023-02-21 01:34:57 -0800

    format

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const temp = document.querySelector('.temp')
 const desc = document.querySelector('desc')
 const wind = document.querySelector('.wind')
 const humidity = document.querySelector('.humidity')
+const icon = document.querySelector('.icon')
 
 
 
@@ -14,17 +15,20 @@ searchButton.addEventListener('click', function(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=`+inputValue.value+`&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         let nameValue = data['name']
         let tempValue = data['main']['temp']
         let descValue = data['weather'][0]['description']
         let windValue = data['wind']['speed']
         let humidityValue = data['main']['humidity']
+        let iconValue = data['weather'][0]['icon']
 
         name.innerHTML = nameValue
         temp.innerHTML = tempValue
         desc.innerHTML = descValue
         wind.innerHTML = windValue
         humidity.innerHTML = humidityValue
+        icon.innerHTML = iconValue
     })
 .catch(err => alert("Invalid city"))
 })
